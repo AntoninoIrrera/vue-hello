45faab8690f29470295822358420a0693e3b0b1d
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-12-12 14:27:03 +0100

    aggiunt abonus img da data vue

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            message: "Ciao a tutti, questo e Vue3"
+            message: "Ciao a tutti, questo e Vue3",
+            srcImg: "https://res.cloudinary.com/practicaldev/image/fetch/s--vbEQHpsU--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/vg0v5vm9a0c1ix6mdp9s.png"
         }
     }
 }).mount("#app")
